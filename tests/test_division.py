ada40edab8c70e54a07d919f4f00bba515337223
William Bailey <80843829+VantaWhite@users.noreply.github.com>
2021-10-03 12:37:52 +0300

    Update test_division.py

--- tests/test_division.py
+++ tests/test_division.py
@@ -4,4 +4,4 @@ import pytest
 
 @pytest.mark.parametrize("num1, num2, result", [(10, 5, 2), (50, 10, 5), (18, 6, 3)])
 def test_division(num1, num2, result):
-    assert num1 / num2 == result
+    assert division(num1, num2) == result
